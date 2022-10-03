3c32bf6cd55f233d42b6a03f75de4b6229f67a19
engkos <engkoskostaman759@gmail.com>
2022-10-04 04:43:03 +0700

    deploykeun

--- pkg/middleware/uploadFile.go
+++ pkg/middleware/uploadFile.go
@@ -122,7 +122,7 @@ func UploadFile(next http.HandlerFunc) http.HandlerFunc {
 		// FormFile returns the first file for the given key `myFile`
 		// it also returns the FileHeader so we can get the Filename,
 		// the Header and the size of the file
-		file, _, err := r.FormFile("attache")
+		file, _, err := r.FormFile("image")
 
 		if err != nil {
 			fmt.Println(err)
